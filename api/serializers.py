70b6b375eaf9f5247472696e29ffed9d70381917
Rob Spears <rob.sp3ars@gmail.com>
2020-06-18 16:29:27 -0400

    Change structure of shoe serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,27 +1,38 @@
-from rest_framework.serializers import ModelSerializer, StringRelatedField
+from rest_framework.serializers import HyperlinkedModelSerializer, StringRelatedField
 from shoes import models
 
 
-class ShoeSerializer(ModelSerializer):
+class ShoeSerializer(HyperlinkedModelSerializer):
+    manufacturer = StringRelatedField()
+    color = StringRelatedField()
+    shoe_type = StringRelatedField()
 
     class Meta:
         model = models.Shoe
-        fields = '__all__'
+        fields = [
+            'size',
+            'brand_name',
+            'material',
+            'fasten_type',
+            'manufacturer',
+            'color',
+            'shoe_type'
+        ]
 
 
-class ShoeTypeSerializer(ModelSerializer):
+class ShoeTypeSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = models.ShoeType
         fields = '__all__'
 
 
-class ShoeColorSerializer(ModelSerializer):
+class ShoeColorSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = models.ShoeColor
         fields = '__all__'
 
 
-class ManufacturerSerializer(ModelSerializer):
+class ManufacturerSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = models.Manufacturer
         fields = '__all__'
