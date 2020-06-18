eb6ca35ce514be177acac0f385e2db7e22ee5232
Rob Spears <rob.sp3ars@gmail.com>
2020-06-18 17:59:07 -0400

    Config cors middelware

--- shoeStore/settings.py
+++ shoeStore/settings.py
@@ -39,17 +39,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'shoes',
-    'api'
+    'api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'https://localhost:3000'
 ]
 
 ROOT_URLCONF = 'shoeStore.urls'
